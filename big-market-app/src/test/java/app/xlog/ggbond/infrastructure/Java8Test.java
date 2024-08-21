663c36bbfd5f9e58a8022f51cff547235f98cb4d
Ziqi Peng <1962883041@qq.com>
2024-08-21 17:58:04 +0800

    test: 增加测试方法

---
+++ big-market-app/src/test/java/app/xlog/ggbond/infrastructure/Java8Test.java
@@ -0,0 +1,17 @@
+package app.xlog.ggbond.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+@SpringBootTest
+public class Java8Test {
+
+    @Test
+    public void test_Optional(){
+        Optional<Object> empty = Optional.empty();
+
+
+    }
+}
