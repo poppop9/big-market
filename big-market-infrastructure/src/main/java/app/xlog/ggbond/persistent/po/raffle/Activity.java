00cfe0c7b5988fec0b59b8eaa0bd0a3dd61176e8
Ziqi Peng <1962883041@qq.com>
2024-12-25 18:30:59 +0800

    refactor : 优化依赖

--- big-market-infrastructure/src/main/java/app/xlog/ggbond/persistent/po/raffle/Activity.java
+++ big-market-infrastructure/src/main/java/app/xlog/ggbond/persistent/po/raffle/Activity.java
@@ -3,10 +3,7 @@ package app.xlog.ggbond.persistent.po.raffle;
 import app.xlog.ggbond.persistent.po.LongListToJsonConverter;
 import cn.hutool.core.util.IdUtil;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
